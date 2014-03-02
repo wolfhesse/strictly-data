be674cebd97368e9beb241cea67df3d1372b80a7
wolfhesse <wolfgang.schuessel@gmail.com>
2014-03-02 16:28:21 +0100

    wp

---
+++ rel.r.d/r20130724.1.R
@@ -0,0 +1,12 @@
+
+library("RJSONIO", lib.loc="/home/alpha/R/i686-pc-linux-gnu-library/2.15")
+library("RCurl", lib.loc="/home/alpha/R/i686-pc-linux-gnu-library/2.15")
+text=eins=topics=docs=c()
+text=getURL("http://service.yasssu.com/proxy_topic/?hitzewelle")
+eins=fromJSON(text)
+docs=eins$response$docs
+for(doc in docs[]) topics=append(topics,doc$topics)
+stopics=sort(topics)
+TopicDataFrame = data.frame(topics=stopics)
+TopicTable = table(TopicDataFrame)
+barplot(sort(TopicTable,decreasing=TRUE)[1:20],horiz=TRUE,cex.names=.45, cex.axis=.2,las=1)
---
+++ rel.r.d/r20130725.1.mongostuff.R
@@ -0,0 +1,9 @@
+
+mongo <- mongo.create()
+if (mongo.is.connected(mongo)) {
+  buf <- mongo.bson.buffer.create()
+  mongo.bson.buffer.append(buf, "name", "Joe")
+  mongo.bson.buffer.append(buf, "age", 22L)
+  b <- mongo.bson.from.buffer(buf)
+  mongo.insert(mongo, "test.people", b)
+}
