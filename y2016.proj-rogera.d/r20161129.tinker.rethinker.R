85905d56a06eeb09b640f5de30b4bd2c86043a09
wolfhesse <wolfhesse@gmail.com>
2017-07-28 12:00:41 +0000

    wp rethinker functions; ctrl-shift-k files: html, pdf

--- y2016.proj-rogera.d/r20161129.tinker.rethinker.R
+++ y2016.proj-rogera.d/r20161129.tinker.rethinker.R
@@ -1,24 +1,52 @@
-d0<- function(){ cn=openConnection(host="rethinkdb.wolfspool.chickenkiller.com",port=28015); return(cn);}
-d1<-function()  r()$db("test")$table("scratchpad")$insert(
-    list(
-      id_previous="779",
-      stuff=list(who="cares",extended='yes'),
-      mode="manual"
-    ),
-    conflict="update",
-    return_changes=TRUE
-  )$run(cn)
-d2<-function() 1
-d3<-function() r()$db("test")$table("products")$insert(
-  list(id=p1$id,
-       title=p1$title,
-       body_html=p1$body_html,
-       handle=p1$handle)
-  ,conflict="update",return_changes=TRUE)$run(cn)
-d4<-function(p1) r()$db("test")$table("products")$insert(
-  list(id=p1$id,
-       title=p1$title,
-       body_html=p1$body_html,
-       handle=p1$handle)
-  ,conflict="update",return_changes=TRUE)$run(cn)
 
+library(rethinker)
+
+rethink_db_host = "rethinkdb.wolfspool.chickenkiller.com" # dep
+rethink_db_host = "base.wolfslab.local.wolfspool.at"      # .1 intern: deutschland
+# admin .here: http://base.wolfslab.local.wolfspool.at:18080/
+
+d0 <-
+  function() {
+    cn = openConnection(host = rethink_db_host, port = 28015)
+    return(cn)
+  }
+
+d1 <- function()
+  r()$db("test")$table("scratchpad_rstudio")$insert(
+    list(
+      id_previous = "779",
+      stuff = list(who = "cares", extended = 'yes'),
+      mode = "manual"
+    ),
+    conflict = "update",
+    return_changes = TRUE
+  )$run(cn)
+
+d2 <- function()
+  1
+
+d3 <- function()
+  r()$db("test")$table("products_rstudio")$insert(
+    list(
+      id = p1$id,
+      title = p1$title,
+      body_html = p1$body_html,
+      handle = p1$handle
+    )
+    ,
+    conflict = "update",
+    return_changes = TRUE
+  )$run(cn)
+
+d4 <- function(p1)
+  r()$db("test")$table("products_rstudio")$insert(
+    list(
+      id = p1$id,
+      title = p1$title,
+      body_html = p1$body_html,
+      handle = p1$handle
+    )
+    ,
+    conflict = "update",
+    return_changes = TRUE
+  )$run(cn)
