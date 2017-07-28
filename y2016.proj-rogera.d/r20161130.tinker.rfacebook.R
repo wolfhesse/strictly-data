188d04d17e60ab8370bbb8325ee542cf2f10f32f
rogera <wolfgang.schuessel@gmail.com>
2017-07-28 21:43:48 +0000

    prep for packae zulu dev
    ase-facebook

--- y2016.proj-rogera.d/r20161130.tinker.rfacebook.R
+++ y2016.proj-rogera.d/r20161130.tinker.rfacebook.R
@@ -1,5 +1,10 @@
 library(Rfacebook)
 
-token<-'EAACEdEose0cBADxRPYfZAedyiIUXSEf44jblSYXfe74L854NfJq3KIZAy25MFOHDtw7q30YcUYlLxRP6jjBVuAc8zZCdWuEQFyVlhIRF0lq36VUl3jOHZBrq8ucXDmweQN9DxnPbOhskrDoxCXU2egjw7tixZAmgWPBaS2bBVYgZDZD'
-me <- getUsers("me", token, private_info=TRUE)
+ALT_TOKEN <-
+  'EAACEdEose0cBADxRPYfZAedyiIUXSEf44jblSYXfe74L854NfJq3KIZAy25MFOHDtw7q30YcUYlLxRP6jjBVuAc8zZCdWuEQFyVlhIRF0lq36VUl3jOHZBrq8ucXDmweQN9DxnPbOhskrDoxCXU2egjw7tixZAmgWPBaS2bBVYgZDZD'
 
+execute <-
+  function(token =
+             ALT_TOKEN) {
+    me <- getUsers("me", token, private_info = TRUE)
+  }
