ed708c005e104b22014027e155caaa5136ad0e86
rogera.dd <wolfgang.schuessel@gmail.com>
2017-09-04 03:27:12 +0200

    wp

--- interactive.d/s_run_dotnetapp-sample.sh
+++ interactive.d/s_run_dotnetapp-sample.sh
@@ -1,24 +1,9 @@
 RESULTFILE="${0%%.sh}.res" 
-exec >> $RESULTFILE  2>&1 && tail $RESULTFILE &
+#exec >>$RESULTFILE 2>&1 
+
 echo .anf
-
-echo "1 - uname"
-echo "  -"
-echo "  - wilder hostname? -> docker image von rstudio"
-uname -a
-
-echo "2 : docker pull"
 docker pull wolfhesse/dotnetapp-sample
-
-echo "3 - docker run"
 docker run --rm wolfhesse/dotnetapp-sample "eins acht ueber" | grep '.here' | jq "."
-<<<<<<< HEAD
-#docker run --rm wolfhesse/dotnetapp-sample "eins acht ueber" 
-=======
-
-
-
->>>>>>> bb4ff3a6d7ebde208b14e0448e38ab04fc01fb72
 echo .fin
 
 
