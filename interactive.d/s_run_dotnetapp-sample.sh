48c52e6c7711dad899b5446306cd966324cac27d
rogera.dd <wolfgang.schuessel@gmail.com>
2017-09-04 02:33:31 +0200

    wp

--- interactive.d/s_run_dotnetapp-sample.sh
+++ interactive.d/s_run_dotnetapp-sample.sh
@@ -1,9 +1,9 @@
-
-
-exec 2>&1 1>>"${0%%.sh}.res"
+RESULTFILE="${0%%.sh}.res" 
+exec >> $RESULTFILE  2>&1 && tail $RESULTFILE &
 echo .anf
 docker pull wolfhesse/dotnetapp-sample
 docker run --rm wolfhesse/dotnetapp-sample "eins acht ueber" | grep '.here' | jq "."
+#docker run --rm wolfhesse/dotnetapp-sample "eins acht ueber" 
 echo .fin
 
 
